3614b955500f3d1d30ea8acdc657995dd6c03acf
Sazanamin <rachel9029@icloud.com>
2022-06-05 04:36:25 +0900

    update: web server
    取得と追加の処理のサンプル

--- server.ts
+++ server.ts
@@ -1,10 +1,46 @@
-// @ts-ignore
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const books = [
+    {
+        id: 1,
+        title: "方法序説",
+        author: "ルネ・デカルト",
+    },
+    {
+        id: 2,
+        title: "良いコード/悪いコードで学ぶ設計入門",
+        author: "仙塲大也",
+    },
+    {
+        id: 3,
+        title: "ゼロの使い魔",
+        author: "ヤマグチノボル",
+    },
+];
+
+const router = new Router();
+
+router
+    .get("/", (context) => {
+        context.response.body = "Hello world!";
+    })
+    .get("/book", (context) => {
+        context.response.body = books;
+    })
+    .get("/book/:id", (context) => (context.response.body = books.find((_) => _.id === Number(context.params.id))))
+    .post("/book", async (context) => {
+        const result = context.request.body({
+            contentTypes: {
+                text: ["application/json"],
+            },
+        });
+
+        const body = await result.value;
+        console.log(body);
+    });
 
 const app = new Application();
-
-app.use((ctx) => {
-    ctx.response.body = "Hello World!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
